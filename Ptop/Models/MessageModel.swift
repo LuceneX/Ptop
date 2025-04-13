6b97a42ecb66bdf207066392d04b3d88ad638b07
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-04-13 21:55:20 +0300

    MessageModel implementation to define the structure and constants of the Message within Chat

--- Ptop/Models/MessageModel.swift
+++ Ptop/Models/MessageModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+//MessaegeModel specifying the message structure
+struct MessageModel: Codable {
+    
+    let id: String // Let's messages be identifieble in order to be able to delete them, sort them in order
+    let sender: String // Sender's public key
+    let recipient: String // Recipient's public key
+    let encryptedMessage: String // Encrypted message
+    let encryptedSymmetricKey: String // Encrypted symmetric key
+    let timestamp: Date // Timestamp of when the message was sent
+    let isRead: Bool // Flag to indicate if the message has been read
+    let isDeleted: Bool // Flag to indicate if the message has been deleted
+    let chatId: String // Identifier for the chat this message belongs to
+    let messageType: MessageType // Type of the message (text, image, video, etc.)
+    

b09202373af15995954fd23138fcc9d7cadb1ce1
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-04-13 21:56:00 +0300

    ChatModel implementation to develop chat identification functionality and define the structure of Chat

--- Ptop/Models/ChatModel.swift
+++ Ptop/Models/ChatModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+//Chat model to store the chat information
+
+struct ChatModel: Codable {
+    /* Chats will be created using Peer to peer network technology, by adding recipients using QR code containing unique chatID containing at least 32 chars, including letters, uppercase letters and digits.*/
+    let id: String // Unique identifier for the chat
+    let participants: [String] // Array of public keys of participants in the chat
+    let lastMessage: MessageModel? // The last message sent in the chat
+    let lastMessageTimestamp: Date? // Timestamp of the last message
+    let unreadCount: Int // Number of unread messages in the chat
+    /*
+    let isGroup: Bool // Flag to indicate if the chat is a group chat 
+     COMING SOON
+     */
+
